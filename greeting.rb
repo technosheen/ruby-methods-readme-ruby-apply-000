63d1148d5e6df8da306d644c02c99237adc01d1b
Sean Mahoney <hello@seanmahoney.design>
2020-07-15 20:59:06 +0000

    Done.

---
+++ greeting.rb
@@ -0,0 +1,19 @@
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+ 
+say_hello_world_ten_times
+ 
+# ... The rest of the program
+ 
+say_hello_world_ten_times
