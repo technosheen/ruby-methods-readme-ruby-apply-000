cd95ea0c21885ef2d9888aafec2f73ed3018a3d9
Sean Mahoney <hello@seanmahoney.design>
2020-07-15 20:58:41 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,9 +1,19 @@
-def greeting
-  puts "Hello World"
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
-
-greeting
-greeting
-greeting
-greeting
-greeting
+ 
+say_hello_world_ten_times
+ 
+# ... The rest of the program
+ 
+say_hello_world_ten_times
